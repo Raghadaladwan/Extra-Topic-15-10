77bc43a5808519541f31e472b372fd9aacef4d85
Raghadaladwan <raghadaledwan97@gmail.com>
2019-10-16 13:01:33 +0300

    Done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,37 +1,116 @@
-import React, {Component} from 'react';
-import {PropTypes} from 'prop-types';
+import React, { Component } from "react";
+// import { PropTypes } from "prop-types";
+import Message from "./Message";
 
 class Form extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isEmailValid: false,
-            isNameValid: false,
-            isPhoneValid: false,
-            isUrlValid: false,
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      isEmailValid: false,
+      isNameValid: false,
+      isPhoneValid: false,
+      isUrlValid: false,
+      submitted: "Form Result Hill Show Here"
+    };
+  }
 
+  handelNameChange = event => {
+    let regular = /^[a-zA-Z]{3,30}/.test(event.target.value);
+
+    this.setState({ isNameValid: regular });
+
+    console.log("isnameValid", regular);
+  };
+
+  emailChange = event => {
+    let regular = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-][a-zA-Z0-9])?)*$/.test(
+      event.target.value
+    );
+    this.setState({ isEmailValid: regular });
+    console.log("this.state.Url", regular);
+  };
+
+  phoneChange = event => {
+    let regular = /^[2-9]{1}[0-9]{9}$/.test(event.target.value);
+    this.setState({ isPhoneValid: regular });
+    console.log("this.state.PhoneValid", regular);
+  };
+
+  BlogURLChange = event => {
+    let regular = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/.test(
+      event.target.value
+    );
+    this.setState({ isUrlValid: regular });
+    console.log("this.state.Ur", regular);
+  };
+
+  handelSubmit = event => {
+    if (
+      this.state.isEmailValid &&
+      this.state.isNameValid &&
+      this.state.isPhoneValid &&
+      this.state.isUrlValid
+    ) {
+      this.setState({ submitted: "Form is completed" });
+    } else {
+      this.setState({ submitted: "Form is NOT completed" });
     }
-    
-    render() {
-        return (
-            <div className="row">
-            <h1 className="text-center">Form Validation</h1>
-            <form>
-                <h3>Name:
-                </h3>
-                <h3>Email:
-                </h3>
-                <h3>Phone:
-                </h3>
-                <h3>Blog URL:
-                </h3>
-                <div className="small-6 small-centered text-center columns">
-                    <a href="#" className="button success expand round text-center">Verify</a>
-                </div>
-            </form>
-        </div>);
-    }
+  };
+
+  render() {
+    const { handelNameChange, emailChange, BlogURLChange, phoneChange,handelSubmit } = this;
+    return (
+      <div className="container">
+        <h1 className="text-center">Form Validation</h1>
+          <h3>
+            Name:
+            <input
+              type="text"
+              placeholder="Name"
+              name="Name"
+              onChange={handelNameChange.bind(this)}
+            />
+          </h3>
+
+          <h3>
+            Email:
+            <input
+              type="email"
+              placeholder="Email"
+              name="Email"
+              onChange={emailChange.bind(this)}
+            />
+          </h3>
+          <h3>
+            Phone:
+            <input
+              type="text"
+              placeholder="Phone"
+              name="Phone"
+              onChange={phoneChange.bind(this)}
+            />
+          </h3>
+          <h3>
+            Blog URL:
+            <input
+              type="text"
+              placeholder="URL"
+              name="URL"
+              onChange={BlogURLChange.bind(this)}
+            />
+          </h3>
+          <div className="small-6 small-centered text-center columns">
+            <button
+              onClick={handelSubmit}
+              className="button success expand round text-center"
+            >
+              submit
+            </button>
+          </div>
+        <Message submitted={this.state.submitted} />
+      </div>
+    );
+  }
 }
 
 export default Form;
